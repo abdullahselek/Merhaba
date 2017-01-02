bc4db63dbae7a61e037e7acae332b457aef50f06
abdullahselek <abdullahselek@gmail.com>
2017-01-02 23:20:46 +0100

    - add iOS target

---
+++ Sources/MRBServer.h
@@ -0,0 +1,96 @@
+//
+//  MRBServer.h
+//  Merhaba
+//
+//  Created by Abdullah Selek on 02/01/2017.
+//
+//  MIT License
+//
+//  Copyright (c) 2017 Abdullah Selek
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+#import <Foundation/Foundation.h>
+
+@class MRBServer;
+
+/**
+  * Protocol for MRBServer
+ */
+@protocol MRBServerDelegate <NSObject>
+
+/**
+  * Both sides of the connection are ready to go
+  *
+  * @param server MRBServer
+ */
+- (void)serverRemoteConnectionComplete:(MRBServer *)server;
+
+/**
+  * server is finished stopping
+  *
+  * @param server MRBServer
+ */
+- (void)serverStopped:(MRBServer *)server;
+
+/**
+  * Server could not start
+  *
+  * @param server MRBServer
+  * @param errorDict NSDictionary
+ */
+- (void)server:(MRBServer *)server didNotStart:(NSDictionary *)errorDict;
+
+/**
+  * Gets data from the remote side of the server
+  *
+  * @param server MRBServer
+  * @param data NSData
+ */
+- (void)server:(MRBServer *)server didAcceptData:(NSData *)data;
+
+/**
+  * connection to the remote side is lost
+  *
+  * @param server MRBServer
+  * @param errorDict NSDictionary
+ */
+- (void)server:(MRBServer *)server lostConnection:(NSDictionary *)errorDict;
+
+/**
+  * A new service comes on line
+  *
+  * @param service NSNetService
+  * @param more BOOL
+ */
+- (void)serviceAdded:(NSNetService *)service moreComing:(BOOL)more;
+
+/**
+  * A service went off line
+  *
+  * @param service NSNetService
+  * @param more BOOL
+ */
+- (void)serviceRemoved:(NSNetService *)service moreComing:(BOOL)more;
+
+@end
+
+@interface MRBServer : NSObject
+
+@end
