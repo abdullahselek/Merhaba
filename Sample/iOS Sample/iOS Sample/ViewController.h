7daf15d28505a6692dfa87da8d41669fea66622e
abdullahselek <abdullahselek@gmail.com>
2017-01-13 21:27:51 +0100

    - create sample project

---
+++ Sample/iOS Sample/iOS Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS Sample
+//
+//  Created by Abdullah Selek on 13/01/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sample/iOS Sample/iOS Sample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS Sample
+//
+//  Created by Abdullah Selek on 13/01/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
