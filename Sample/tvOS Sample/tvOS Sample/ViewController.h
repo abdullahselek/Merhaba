161ac360be9dfa970f693cfcc24d0307101ee244
abdullahselek <abdullahselek@gmail.com>
2017-01-28 14:00:19 +0100

    - create tvOS sample project

---
+++ Sample/tvOS Sample/tvOS Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tvOS Sample
+//
+//  Created by Abdullah Selek on 28/01/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sample/tvOS Sample/tvOS Sample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  tvOS Sample
+//
+//  Created by Abdullah Selek on 28/01/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
