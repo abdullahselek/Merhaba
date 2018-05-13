a4ec31e1a2c7d7b1ffa1522a865a4cd1fac5b216
Abdullah Selek <abdullahselek@gmail.com>
2018-05-13 10:27:49 +0100

    Update Xcode settings and ci.

---
+++ Pods/Target Support Files/Merhaba-macOS/Merhaba-macOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Merhaba_macOS : NSObject
+@end
+@implementation PodsDummy_Merhaba_macOS
+@end
--- Pods/Target Support Files/OCMock-OSX/OCMock-OSX-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_OCMock_OSX : NSObject
-@end
-@implementation PodsDummy_OCMock_OSX
-@end
